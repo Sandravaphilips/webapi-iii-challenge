be523dbdcb79343b3912f809a8e2aabf6b272080
Sandrava Philips <lacasandra1@gmail.com>
2019-10-31 12:56:13 +0100

    one stretch task completed

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoutes = require('./users/userRouter');
+const postRoutes = require('./posts/postRouter');
 
 const server = express();
 
 server.use(express.json());
 server.use('/users', logger, userRoutes);
+server.use('/posts', logger, postRoutes)
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
